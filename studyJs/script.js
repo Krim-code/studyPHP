cf69691d4f6bdad4cafd801a36c7da421f36e25c
AntoxaElros <antoxa11091984@ya.ru>
2023-08-09 10:54:06 +0300

    r

--- studyJs/script.js
+++ studyJs/script.js
@@ -9,10 +9,12 @@ function draw(a) {
     const body = document.body
     
 
-    for (let index = 0; index < a; index++) {
+    
+        for (let index = 0; index < a; index++) {
+        setTimeout(()=>{
         let justDiv = document.createElement("div")
         justDiv.classList.add('justDiv')
         justDiv.innerHTML = index;
         body.appendChild(justDiv)
-    }
+        } ,"1000")}
 }
